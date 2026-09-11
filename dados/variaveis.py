bd667b61387208c20fb66c718cf32d67da35d9b4
Lorran C. S. <lorrandesenvolvedor@gmail.com>
2026-09-11 09:32:56 -0300

    Add files via upload

---
+++ dados/variaveis.py
@@ -0,0 +1,12 @@
+VARIAVEIS = {
+    "START": "\n estou a disposição para contar seus gastos, abaixo tenho meu menu de opções:\n\n"
+             "/limite = use esse comando para definir um limite\n\n"
+             "/gasto = use esse comando para definir um gasto\n\n"
+             "exemplos:\n/limite 3.000 ou /gasto 21.00"
+}
+
+CODIGO = {
+
+    "codigo": "SUA KEY DO TELEGRAM"
+
+}
